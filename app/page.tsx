e5c400dadcd5618892330acde1b5e73ba9d908b7
Arjay Legaspi <alegaspi@cloud816.com>
2026-04-09 05:55:54 +0800

    Fix TypeScript implicit any error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 export default async function Home() {
-  const res = await fetch("https://griffinheadlesscms.kinsta.cloud/wp-json/wp/v2/pages");
+  const res = await fetch("https://griffinheadlesscms.kinsta.cloud/wp-json/wp/v2/pages", {
+    cache: "no-store",
+  });
   const data = await res.json();
 
   return (
     <div>
       <h1>Griffin Headless Site</h1>
-      {data.map((page) => (
+      {data.map((page: any) => (
         <div key={page.id}>
           <h2 dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
         </div>
