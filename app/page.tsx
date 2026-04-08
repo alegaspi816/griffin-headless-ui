d14b9ac90caf5be965e0706ed6fa2467305da7f7
Arjay Legaspi <alegaspi@cloud816.com>
2026-04-09 06:00:19 +0800

    Fix TypeScript typing for WP pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+type WPPage = {
+  id: number;
+  title: {
+    rendered: string;
+  };
+};
+
 export default async function Home() {
   const res = await fetch("https://griffinheadlesscms.kinsta.cloud/wp-json/wp/v2/pages", {
     cache: "no-store",
   });
-  const data = await res.json();
+
+  const data: WPPage[] = await res.json();
 
   return (
     <div>
       <h1>Griffin Headless Site</h1>
-      {data.map((page: any) => (
+      {data.map((page) => (
         <div key={page.id}>
           <h2 dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
         </div>
